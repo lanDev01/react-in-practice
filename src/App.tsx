62e9deca612ac783c64aef7c5567448210f9f7e6
LanDev01 <alnjr0001@outlook.com>
2024-02-22 10:41:28 -0300

    base ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import { Plus } from 'lucide-react'
+import { FileDown, MoreHorizontal, Plus, Search } from 'lucide-react'
 import { Header } from './components/header'
 import { Tabs } from './components/tabs'
+import { Button } from './components/ui/button'
+import { Control, Input } from './components/ui/input'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table'
+// import { Pagination } from './components/pagination'
 
 export function App() {
   return (
@@ -12,15 +16,58 @@ export function App() {
       <main className="max-w-6xl mx-auto space-y-5">
         <div className='flex items-center gap-3'>
           <h1 className="text-xl font-bold">Tags</h1>
-          <button className='inline-flex items-center gap-1.5 text-xs bg-teal-300 text-teal-950 font-medium rounded-full px-1.5 py-1'>
+          <Button variant='primary'>
             <Plus className='size-3' />
             Create New
-          </button>
+          </Button>
         </div>
 
         <div className='flex items-center justify-between'>
-          <div></div>
+          <Input variant='filter'>
+            <Search className="size-3"/>
+            <Control placeholder='Search tags'/>
+          </Input>
+
+          <Button>
+            <FileDown className='size-3' />
+            Export
+          </Button>
         </div>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead></TableHead>
+              <TableHead>Tag</TableHead>
+              <TableHead>Amount of videos</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({length: 10}).map((value, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell></TableCell>
+                  <TableCell>
+                    <div className='flex flex-col'>
+                      <span className='font-medium'>React</span>
+                      <span className='text-xs text-zinc-500'>fec777f2-81fd-4237-bbaf-9446fa08797c</span>
+                    </div>
+                  </TableCell>
+                  <TableCell className='text-zinc-300'>
+                    13 videos(s)
+                  </TableCell>
+                  <TableCell className='text-right'>
+                    <Button size='icon'>
+                      <MoreHorizontal className='size-4' />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+        
+        {/* <Pagination /> */}
       </main>
     </div>
   )
